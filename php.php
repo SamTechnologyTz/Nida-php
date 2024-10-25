15328b738c9c547c2cf9562b3d434e1916b6a879
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-10-25 17:15:17 +0300

    Add files via upload
    Signed-off-by: SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>

---
+++ php.php
@@ -0,0 +1,34 @@
+<?php
+$url = "http://localhost/".$_SERVER['HTTP_HOST']."/nida/api";
+$account_id = "Your account id";
+$api_key = "Your api key ";
+$token = "Your token";
+$nida = "your nida";
+
+$data = [
+    'nida' => $nida
+];
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($curl, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+    'api-key:'.$api_key,
+    'token:'.$token,
+    'account-id:'.$account_id,
+]);
+
+$response = curl_exec($curl);
+$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+if ($response === false) {
+    $response = json_encode(['status' => 'Error', 'message' => 'There is an Erroe '.curl_error($curl)]);
+    echo $response;
+} elseif ($httpCode === 200) {
+    $response = json_encode($response, true);
+    echo $response;
+}
+
+?>
